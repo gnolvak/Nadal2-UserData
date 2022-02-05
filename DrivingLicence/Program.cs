c669bf6002f4c166dfcf59a4ff8af50af67c04f2
Gregor Nõlvak <gregor.nolvak@gmail.com>
2022-02-05 17:54:32 +0200

    Control Flow

---
+++ DrivingLicence/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DrivingLicence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajalt sünniaastat
+            //programm arvutab kasutaja vanust
+            //kui kasutaja on noorem, kui 18, siis programm kuvab konsoolis
+            //"oled liiga noor, et juhilube saada";
+            //kui kasutaja on vanem, kui 18, siis programm kuvab konsoolis
+            //"oled piisavalt vana, et juhilube saada";
+            //kui kasutaja on 18, siis programm kuvab konsoolis
+            //"Palju õnne! Nüüd sa saad juhilube taotleda"
+
+            Console.WriteLine("Palun, sisesta oma sünniaasta.");
+
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2022 - yearOfBirth;
+
+            if (userAge > 18)
+            {
+                Console.WriteLine("Oled piisavalt vana, et juhilube saada");
+            }
+
+            else if (userAge < 18) 
+            {
+                Console.WriteLine("Oled liiga noor, et juhilube saada");
+            }
+            
+            else 
+            {
+                Console.WriteLine("Palju õnne, nüüd sa saad juhilube taotleda");
+            }
+
+            Console.WriteLine("Kena päeva!");
+
+            
+        }
+    }
+}
